7c9127a3f0dc85d919dd77bb67adeed3e62120f5
MathiasWickenhagen <mathiaswickenhagen@web.de>
2024-08-08 20:40:14 +0200

    feat: Add pathOrLoader by TypedYamlConfig

--- src/TypedConfig.module.ts
+++ src/TypedConfig.module.ts
@@ -1,10 +1,11 @@
 import { ConfigModule, registerAs } from "@nestjs/config";
-import { ClassConstructor } from "class-transformer/types/interfaces";
+import { ClassConstructor, ClassTransformOptions } from "class-transformer/types/interfaces";
 import { validateConfigUtils } from "./utils/validateConfig.utils";
 import * as process from "process";
-import { readFileSync } from "fs";
-import { join } from "path";
+import { PathLike, readFileSync } from "fs";
+import { isAbsolute, join } from "path";
 import { load } from "js-yaml";
+import { ValidatorOptions } from "class-validator/types/validation/ValidatorOptions";
 
 export class TypedConfig {
   static forFeature<T extends object>(cls: ClassConstructor<T>) {
@@ -15,16 +16,40 @@ export class TypedConfig {
 }
 
 export class TypedYamlConfig {
-  static forFeature<T extends object>(cls: ClassConstructor<T>, path: string, ignoreWhitelist: boolean = false) {
+  static forFeature<T extends object>(cls: ClassConstructor<T>,
+                                      pathOrLoader: PathLike | (() => Promise<string> | string),
+                                      transformOptions: ClassTransformOptions = {},
+                                      validatorOptions: ValidatorOptions = {}) {
     return ConfigModule.forFeature(registerAs(cls.name, async () => {
-        const rootPath: string = process.cwd();
-        const file: string = readFileSync(join(rootPath, path), 'utf8');
-        const object: object = await load(file) as object;
-        return validateConfigUtils(object, cls, {
-          strategy: 'exposeAll',
-        }, {
-          whitelist: !ignoreWhitelist,
-        });
+
+        let yaml: string;
+
+        if (typeof pathOrLoader === 'function') { // Custom loader
+          yaml = await pathOrLoader();
+
+        } else if (typeof pathOrLoader !== 'string') { // Load by Buffer or URL
+          yaml = readFileSync(pathOrLoader, 'utf8');
+
+        } else if (isAbsolute(pathOrLoader)) { // Load by absolute path
+          yaml = readFileSync(pathOrLoader, 'utf8');
+
+        } else { // Load by relative path
+          const rootPath: string = process.cwd();
+          yaml = readFileSync(join(rootPath, pathOrLoader), 'utf8');
+        }
+
+        // YAML-TO-JSON Parser
+        const jsonObject: object = await load(yaml) as object;
+
+        // Check validation
+        return validateConfigUtils(jsonObject,
+          cls,
+          Object.assign({
+            strategy: 'exposeAll',
+          }, transformOptions),
+          Object.assign({
+            whitelist: true,
+          }, validatorOptions));
       })
     )
   }
